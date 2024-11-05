fe758c30453513a7752f205738cfaaf154c4786f
Choiseungyeop <tmdduq04@naver.com>
2024-11-05 09:20:54 +0900

    Delete gamSearch directory

--- gamSearch/src/main/java/com/gamesearch/domain/community/CommunityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gamesearch.domain.community;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommunityRepository extends JpaRepository<Community, Long> {
-    List<Community> findByCategory(String category);
-}
