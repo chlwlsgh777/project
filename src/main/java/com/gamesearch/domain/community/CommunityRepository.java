7adf505fc335877d29efd9627ec9c388d0e6a531
choiseungyeop <tmdduq04@gmail.com>
2024-11-13 15:37:53 +0900

    마이페이지 작성한 게시글 추가

--- src/main/java/com/gamesearch/domain/community/CommunityRepository.java
+++ src/main/java/com/gamesearch/domain/community/CommunityRepository.java
@@ -3,12 +3,16 @@ package com.gamesearch.domain.community;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gamesearch.domain.user.User;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface CommunityRepository extends JpaRepository<Community, Long> {
     List<Community> findByCategory(String category);
     List<Community> findByTitleContainingIgnoreCase(String title);
+    List<Community> findByAuthor(User author);
     Page<Community> findByTitleContainingIgnoreCase(String title, Pageable pageable);
     Page<Community> findByCategory(String category, Pageable pageable);
     Page<Community> findByCategoryAndTitleContainingIgnoreCase(String category, String title, Pageable pageable);
