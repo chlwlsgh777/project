ff9a21a91c9686f9ce86716fa12026a76fb685cf
Choiseungyeop <tmdduq04@naver.com>
2024-11-25 09:49:42 +0900

    이메일 표현식 변경

--- src/main/java/com/gamesearch/domain/user/UserService.java
+++ src/main/java/com/gamesearch/domain/user/UserService.java
@@ -17,7 +17,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
     private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
     @Transactional
